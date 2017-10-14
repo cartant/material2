364d90c466d6107a974cb55b5ee2ecb55f28d70e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-10-14 14:02:29 +0200

    feat(selection-list): support for ngModel
    * Adds support for NgModel to the selection-list.
    
    Fixes #6896

--- src/demo-app/list/list-demo.ts
+++ src/demo-app/list/list-demo.ts
@@ -51,4 +51,13 @@ export class ListDemo {
 
   thirdLine: boolean = false;
   infoClicked: boolean = false;
+
+  selectedOptions: string[] = ['apples'];
+  changeEventCount: number = 0;
+  modelChangeEventCount: number = 0;
+
+  onSelectedOptionsChange(values: string[]) {
+    this.selectedOptions = values;
+    this.modelChangeEventCount++;
+  }
 }
